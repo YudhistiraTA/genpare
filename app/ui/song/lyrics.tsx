8c47a76b9280558625ce6cf4a6aaee6f7823d26d
Yudhistira Tribuana Authar <yudhistira.tribuana@gmail.com>
2023-11-30 13:45:44 +0700

    Minor lyrics styling

--- app/ui/song/lyrics.tsx
+++ app/ui/song/lyrics.tsx
@@ -4,9 +4,9 @@ export function Lyrics({ main, sub }: { main?: string; sub?: string }) {
 	const subArray = sub.split('\\n')
 	if (mainArray.length !== subArray.length) return <p>Invalid format</p>
 	return mainArray.map((line, index) => (
-		<ruby key={`lyrics line ${index}`} className="text-xl">
+		<ruby key={`lyrics line ${index}`} className="lg:text-xl my-1">
 			{line}
-			<rt className="text-sm opacity-70">{subArray[index]}</rt>
+			<rt className="text-xs lg:text-sm opacity-70">{subArray[index]}</rt>
 		</ruby>
 	))
 }

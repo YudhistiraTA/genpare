a19bb94b23d9f7e2a18a5e20902a232cb93f6103
Yudhistira <yudhistira.tribuana@gmail.com>
2023-12-07 05:51:09 +0700

    Add 'KR' as a language code

--- app/lib/languageCode.ts
+++ app/lib/languageCode.ts
@@ -1,8 +1,9 @@
 import { $Enums } from '@prisma/client'
-type Code = 'JP' | 'EN' | 'SP' | 'FR'
+type Code = 'JP' | 'EN' | 'SP' | 'FR' | 'KR'
 export const languageCode = new Map<$Enums.Language, Code>([
 	['japanese', 'JP'],
 	['english', 'EN'],
 	['french', 'FR'],
 	['spanish', 'SP'],
+	['korean', 'KR'],
 ])

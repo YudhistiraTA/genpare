65902a3414cba428cd50b6aaf061721f0bb6b862
Yudhistira Tribuana Authar <yudhistira.tribuana@gmail.com>
2024-01-05 14:43:19 +0700

    Renamed loginform

--- app/cms/login/page.tsx
+++ app/cms/login/page.tsx
@@ -1,4 +1,4 @@
-import { LoginForm } from '@/app/ui/cms/loginForm'
+import { LoginForm } from '@/app/ui/cms/LoginForm'
 import { Metadata } from 'next'
 export const metadata: Metadata = {
 	title: 'Login',

6498e87fa922b63a3b3cc04f2207f666074aca49
Yudhistira Tribuana Authar <yudhistira.tribuana@gmail.com>
2024-02-15 10:59:47 +0700

    Removed unused components

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,26 +1,5 @@
-import { CardSkeleton } from '@/app/ui/main/cardSkeleton'
-import { MainPageCardWrapper } from '@/app/ui/main/mainPageCardWrapper'
-
-import { Suspense } from 'react'
-
-export default function Page({
-	searchParams: { query },
-}: {
-	searchParams: { query: string }
-}) {
+export default function Page() {
 	return (
-		<main className="flex flex-col items-center justify-between px-2 lg:px-24">
-			<Suspense
-				fallback={
-					<section className="lg:flex grid lg:gap-8 gap-4 my-4 flex-wrap justify-center">
-						{[...Array(6)].map((_, i) => (
-							<CardSkeleton key={`card skeleton ${i}`} />
-						))}
-					</section>
-				}
-			>
-				<MainPageCardWrapper query={query} />
-			</Suspense>
-		</main>
+		<main className="flex flex-col items-center justify-between px-2 lg:px-24"></main>
 	)
 }
